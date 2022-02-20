2c0e7d23d9ced89d2a9e3ccc0245de9fa4bca018
Albert Ruan <berty.ruan@gmail.com>
2022-02-20 11:42:47 +0800

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+function Attribution(props) {
+    return (
+        <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="www.github.com/bertyruan">Berty Ruan</a>.
+        </div>
+    );
+}
+
+class InteractiveCommentsPage extends React.Component {
+    render() {
+        return <Attribution />;
+    }
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <InteractiveCommentsPage />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
